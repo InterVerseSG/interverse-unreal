78b0fea35d77027dfc3e5d31832c7e1458d0feae
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2026-08-27 15:54:56 -0400

    Create runtime Enhanced Input bindings for Meta Quest Touch

--- Source/InterVerseRuntime/Private/InterVerseXRPawn.cpp
+++ Source/InterVerseRuntime/Private/InterVerseXRPawn.cpp
@@ -3,6 +3,11 @@
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "Components/SceneComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/PlayerController.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 #include "InterVerseCloudClient.h"
 #include "InterVerseNavigationComponent.h"
 #include "InterVerseVRLocomotionComponent.h"
@@ -33,10 +38,77 @@ AInterVerseXRPawn::AInterVerseXRPawn()
     Locomotion = CreateDefaultSubobject<UInterVerseVRLocomotionComponent>(TEXT("InterVerseVRLocomotion"));
 }
 
+void AInterVerseXRPawn::BeginPlay()
+{
+    Super::BeginPlay();
+    EnsureEnhancedInputMappings();
+}
+
+void AInterVerseXRPawn::EnsureEnhancedInputMappings()
+{
+    if (!bEnableRuntimeQuestMappings)
+    {
+        return;
+    }
+
+    if (!RuntimeMappingContext)
+    {
+        RuntimeMappingContext = NewObject<UInputMappingContext>(this, TEXT("IV_RuntimeQuestMapping"));
+
+        MoveForwardAction = NewObject<UInputAction>(this, TEXT("IV_IA_MoveForward"));
+        MoveForwardAction->ValueType = EInputActionValueType::Axis1D;
+        MoveRightAction = NewObject<UInputAction>(this, TEXT("IV_IA_MoveRight"));
+        MoveRightAction->ValueType = EInputActionValueType::Axis1D;
+        TurnAction = NewObject<UInputAction>(this, TEXT("IV_IA_Turn"));
+        TurnAction->ValueType = EInputActionValueType::Axis1D;
+        TeleportAction = NewObject<UInputAction>(this, TEXT("IV_IA_Teleport"));
+        TeleportAction->ValueType = EInputActionValueType::Boolean;
+
+        // Meta/Oculus Touch keys exposed by Unreal's OpenXR/Oculus controller profiles.
+        RuntimeMappingContext->MapKey(MoveForwardAction, FKey(FName(TEXT("OculusTouch_Left_Thumbstick_Y"))));
+        RuntimeMappingContext->MapKey(MoveRightAction, FKey(FName(TEXT("OculusTouch_Left_Thumbstick_X"))));
+        RuntimeMappingContext->MapKey(TurnAction, FKey(FName(TEXT("OculusTouch_Right_Thumbstick_X"))));
+        RuntimeMappingContext->MapKey(TeleportAction, FKey(FName(TEXT("OculusTouch_Right_Trigger_Click"))));
+    }
+
+    APlayerController* PC = Cast<APlayerController>(GetController());
+    if (!PC || !PC->GetLocalPlayer() || !RuntimeMappingContext)
+    {
+        return;
+    }
+
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = PC->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+    {
+        Subsystem->AddMappingContext(RuntimeMappingContext, 10);
+    }
+}
+
+void AInterVerseXRPawn::BindEnhancedInput(UInputComponent* PlayerInputComponent)
+{
+    UEnhancedInputComponent* Enhanced = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+    if (!Enhanced || !MoveForwardAction || !MoveRightAction || !TurnAction || !TeleportAction)
+    {
+        return;
+    }
+
+    Enhanced->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &AInterVerseXRPawn::EnhancedMoveForward);
+    Enhanced->BindAction(MoveForwardAction, ETriggerEvent::Completed, this, &AInterVerseXRPawn::EnhancedMoveForward);
+    Enhanced->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &AInterVerseXRPawn::EnhancedMoveRight);
+    Enhanced->BindAction(MoveRightAction, ETriggerEvent::Completed, this, &AInterVerseXRPawn::EnhancedMoveRight);
+    Enhanced->BindAction(TurnAction, ETriggerEvent::Triggered, this, &AInterVerseXRPawn::EnhancedTurn);
+    Enhanced->BindAction(TurnAction, ETriggerEvent::Completed, this, &AInterVerseXRPawn::EnhancedTurn);
+    Enhanced->BindAction(TeleportAction, ETriggerEvent::Started, this, &AInterVerseXRPawn::EnhancedTeleportStarted);
+    Enhanced->BindAction(TeleportAction, ETriggerEvent::Completed, this, &AInterVerseXRPawn::EnhancedTeleportCompleted);
+}
+
 void AInterVerseXRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    EnsureEnhancedInputMappings();
+    BindEnhancedInput(PlayerInputComponent);
+
+    // Legacy fallback remains useful for desktop keyboard/gamepad testing.
     PlayerInputComponent->BindAxis(TEXT("IV_MoveForward"), this, &AInterVerseXRPawn::InputMoveForward);
     PlayerInputComponent->BindAxis(TEXT("IV_MoveRight"), this, &AInterVerseXRPawn::InputMoveRight);
     PlayerInputComponent->BindAxis(TEXT("IV_Turn"), this, &AInterVerseXRPawn::InputTurn);
@@ -108,4 +180,32 @@ void AInterVerseXRPawn::InputTeleportReleased()
     {
         Locomotion->CommitTeleport();
     }
+}
+
+void AInterVerseXRPawn::EnhancedMoveForward(const FInputActionValue& Value)
+{
+    InputMoveForward(Value.Get<float>());
+}
+
+void AInterVerseXRPawn::EnhancedMoveRight(const FInputActionValue& Value)
+{
+    InputMoveRight(Value.Get<float>());
+}
+
+void AInterVerseXRPawn::EnhancedTurn(const FInputActionValue& Value)
+{
+    InputTurn(Value.Get<float>());
+}
+
+void AInterVerseXRPawn::EnhancedTeleportStarted(const FInputActionValue& Value)
+{
+    if (Value.Get<bool>())
+    {
+        InputTeleportPressed();
+    }
+}
+
+void AInterVerseXRPawn::EnhancedTeleportCompleted(const FInputActionValue& Value)
+{
+    InputTeleportReleased();
 }
