3554a2f2182fe17840b879cbe03ecba1ff8d6e2f
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2026-08-27 11:25:00 -0400

    Add Unreal runtime module dependencies

---
+++ Source/InterVerseRuntime/InterVerseRuntime.Build.cs
@@ -0,0 +1,20 @@
+using UnrealBuildTool;
+
+public class InterVerseRuntime : ModuleRules
+{
+    public InterVerseRuntime(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "HTTP",
+            "Json",
+            "JsonUtilities",
+            "DeveloperSettings"
+        });
+    }
+}
