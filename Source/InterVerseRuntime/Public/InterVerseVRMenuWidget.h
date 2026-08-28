0e459857124e83015cfe645d06cb56a6c84e7ed2
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2026-08-28 11:30:44 -0400

    Add runtime Quest VR destination menu widget

---
+++ Source/InterVerseRuntime/Public/InterVerseVRMenuWidget.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InterVerseVRMenuWidget.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class INTERVERSERUNTIME_API UInterVerseVRMenuWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    virtual void NativeConstruct() override;
+
+    UFUNCTION(BlueprintCallable, Category="InterVerse|VR|Menu")
+    void SetStatusText(const FString& Message);
+
+private:
+    void StartGuidance(const FString& NavigationAnchor, const FString& DisplayName);
+
+    UFUNCTION() void GuideMarquis();
+    UFUNCTION() void GuideCAI();
+    UFUNCTION() void GuideStudentCenter();
+    UFUNCTION() void GuideGraduateSchool();
+    UFUNCTION() void CloseMenu();
+
+    UPROPERTY(Transient)
+    TObjectPtr<UTextBlock> StatusText;
+};
