5c92e92484760bb9dd16cb91012cec1fb6e2eba3
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2026-08-28 11:47:09 -0400

    Load all campus destinations dynamically in Quest VR menu

--- Source/InterVerseRuntime/Public/InterVerseVRMenuWidget.h
+++ Source/InterVerseRuntime/Public/InterVerseVRMenuWidget.h
@@ -5,7 +5,20 @@
 #include "InterVerseTypes.h"
 #include "InterVerseVRMenuWidget.generated.h"
 
+class UComboBoxString;
+class UEditableTextBox;
 class UTextBlock;
+
+USTRUCT()
+struct FInterVerseVRMenuDestination
+{
+    GENERATED_BODY()
+
+    FString Id;
+    FString DisplayName;
+    FString NavigationAnchor;
+    FString Category;
+};
 
 UCLASS()
 class INTERVERSERUNTIME_API UInterVerseVRMenuWidget : public UUserWidget
@@ -19,12 +32,18 @@ public:
     void SetStatusText(const FString& Message);
 
 private:
+    bool LoadDestinations();
+    FString DisplayNameForId(const FString& Id, const FString& ExplicitDisplayName) const;
+    FString CategoryForDestination(const FString& Id, const FString& DisplayName) const;
+    void RebuildCategoryOptions();
+    void RebuildDestinationOptions();
+    const FInterVerseVRMenuDestination* FindSelectedDestination() const;
     void StartGuidance(const FString& NavigationAnchor, const FString& DisplayName);
 
-    UFUNCTION() void GuideMarquis();
-    UFUNCTION() void GuideCAI();
-    UFUNCTION() void GuideStudentCenter();
-    UFUNCTION() void GuideGraduateSchool();
+    UFUNCTION() void HandleCategoryChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+    UFUNCTION() void HandleSearchChanged(const FText& Text);
+    UFUNCTION() void GuideSelectedDestination();
+    UFUNCTION() void StopActiveGuidance();
     UFUNCTION() void AskGraduateSchoolAI();
     UFUNCTION() void HandleValidatedCommand(const FInterVerseValidatedCommand& Command);
     UFUNCTION() void HandleCloudError(const FString& Message);
@@ -32,4 +51,19 @@ private:
 
     UPROPERTY(Transient)
     TObjectPtr<UTextBlock> StatusText;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UComboBoxString> CategoryCombo;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UComboBoxString> DestinationCombo;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UEditableTextBox> SearchBox;
+
+    UPROPERTY(Transient)
+    TArray<FInterVerseVRMenuDestination> Destinations;
+
+    FString ActiveCategory = TEXT("Todos");
+    FString SearchFilter;
 };
